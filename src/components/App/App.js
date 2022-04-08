baabf8720e6be0446443915b647f7903e0eb65fb
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2022-04-08 16:06:12 +0400

    added NotFound component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,41 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../pages/Register';
 import Login from '../pages/Login/Login';
+import NotFound from '../NotFound/NotFound'
 import './App.css';
 
 function App() {
+  let history = useHistory();
   return (
     <div className="app">
-      <Route exact path="/">
-        <Main />
-      </Route>
-      <Route path="/movies">
-        <Movies />
-      </Route>
-      <Route path="/saved-movies">
-        <SavedMovies />
-      </Route>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-      <Route path="/signup">
-        <Register />
-      </Route>
-      <Route path="/signin">
-        <Login />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/saved-movies">
+          <SavedMovies />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/signup">
+          <Register />
+        </Route>
+        <Route path="/signin">
+          <Login />
+        </Route>
+        <Route path="*">
+          <NotFound history={history} />
+        </Route>
+      </Switch>
     </div >
   );
 }
