ff8478edfec7d983a9394c1ba7a946e76a3bc24b
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2022-04-20 15:40:07 +0400

    added all functional

--- src/initialData.js
+++ src/initialData.js
@@ -8,7 +8,7 @@ const obj = {
   "duration": 61,
   "description": "В конце 1960-х группа «Роллинг Стоунз», несмотря на все свои мегахиты и сверхуспешные концертные туры, была разорена. Виной всему — бездарный менеджмент и драконовское налогообложение в Британии. Тогда музыканты приняли не самое простое для себя решение: летом 1971 года после выхода альбома «Stiсky Fingers» они отправились на юг Франции записывать новую пластинку. Именно там, на Лазурном Берегу, в арендованном Китом Ричардсом подвале виллы Неллькот родился сборник «Exile on Main St.», который стал лучшим альбомом легендарной группы.",
   "trailerLink": "https://www.youtube.com/watch?v=UXcqcdYABFw",
-  "image": "https://api.nomoreparties.co/beatfilm-movies/thumbnail_stones_in_exile_b2f1b8f4b7",
-  "thumbnail": "https://api.nomoreparties.co/beatfilm-movies/thumbnail_stones_in_exile_b2f1b8f4b7",
+  "image": "https://api.nomoreparties.co/uploads/stones_in_exile_b2f1b8f4b7.jpeg",
+  "thumbnail": "https://api.nomoreparties.co/uploads/thumbnail_stones_in_exile_b2f1b8f4b7.jpeg",
   "movieId": 2
 }
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.siarhey.nomoredomains.work';
 
 export const handleResponse = (res) => {
   if (res.ok) return res.json();
